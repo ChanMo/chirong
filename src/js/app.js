8941936c491e1352e4ae348b50c139c70ed55c51
ChanMo <chen.orange@aliyun.com>
2016-10-17 17:49:50 +0800

    add animate

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,32 @@ $(".menu-toggle").bind("click", function(e){
 });
 
 $("#nav").onePageNav({
-  currentClass: 'active'
+  currentClass: 'active',
+  begin: function(){
+    if($(".small-header").is(":visible")){
+      $("#nav").hide();
+    }
+  }
+});
+
+var map = new AMap.Map('map', {
+  resizeEnable: false,
+  zoom:15,
+  center: [117.122692,36.687315],
+  mapStyle: 'dark',
+  dragEnable: false,
+  zoomEnable: false,
+  touchZoom: false,
+  scrollWheel: false
+});
+
+var marker = new AMap.Marker({
+  position: [117.122692,36.687315],
+  map:map
+});
+
+$(".animated").viewportChecker({
+  classToAdd: 'fadeInUp',
+  repeat: true
 });
 
